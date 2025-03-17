f7c524cfe0aa15e6384cb06e7f0fa802bf85f52d
CJ <cjthejuggler@gmail.com>
2025-03-17 15:59:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 setCookie = (cName, cValue, expDays) => {
     let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + (expDays*24*60*60*1000));
     const expires = "expires=" + date.toUTCString();
     document.cookie = cName + "=" + cValue + "; " + expires + "path=/";
 }
 
 document.querySelector("#c-button").addEventListener("click", () => {
     document.querySelector("#cookies").style.display = "none";
-    setCookie("cookie", true, 90);
+    setCookie("cookie", true, 30);
 })
