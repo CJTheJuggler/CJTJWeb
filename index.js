0f3488b38ab2838110da5f96b6b39beab56e3148
CJ <cjthejuggler@gmail.com>
2025-03-22 15:17:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-setCookie = (cName, cValue, expDays) => {
-    let date = new Date();
-    date.setTime(date.getTime() + (expDays*24*60*60*1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires + "path=/";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JS Loaded")
+    setCookie = (cName, cValue, expDays) => {
+        let date = new Date();
+        date.setTime(date.getTime() + (expDays*24*60*60*1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = cName + "=" + cValue + "; " + expires + "path=/";
+    }
+    
+    cookieMessage = () => {
+        if (!getCookie("cookie"))
+            document.querySelector("#cookie").style.display = "block";
+    }
+    
+    window.addEventListener("load", cookieMessage);
 
-getCookie = (cName) => {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie);
-    const cArr = cDecoded.split("; ");
-    let value;
-    cArr.forEach(val => {
-        if(val.indexOf(name) === 0) value = val.substring(name.length);
-    })
+    if(localStorage.getItem(setCookie) === null) {
 
-    return value;
-}
+        document.querySelector("#c-button-a").addEventListener("click", () => {
+            document.querySelector("#cookies").style.display = "none";
+            document.querySelector(".blur-box").style.display = "none";
+            setCookie("cookie", true, 90);
+            console.log("Cookies Logged");
+        })
 
-document.querySelector("#c-button").addEventListener("click", () => {
-    document.querySelector("#cookies").style.display = "none";
-    setCookie("cookie", true, 90);
+        
+
+    }else {
+        getCookie = (cName) => {
+            const name = cName + "=";
+            const cDecoded = decodeURIComponent(document.cookie);
+            const cArr = cDecoded.split("; ");
+            let value;
+            cArr.forEach(val => {
+                if(val.indexOf(name) === 0) value = val.substring(name.length);
+            })
+        
+            return value;
+        }
+    }
 })
-
-cookieMessage = () => {
-    if (!getCookie("cookie"))
-        document.querySelector("#cookie").style.display = "block";
-}
-
-window.addEventListener("load", cookieMessage);
