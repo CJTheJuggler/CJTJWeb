dcc7a7375021b623d8a9af3136d366790ece304b
CJ <cjthejuggler@gmail.com>
2025-03-24 20:04:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("JS Loaded")
-    function setCookie(cname, cvalue, exdays) {
-        const d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        let expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    setCookie(cname, cvalue, exdays) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = cName + "=" + (value || "") + expires + "; path=/";
     }
 
-    function getCookie(cname) {
-        let name = cname + "=";
-        let ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
+
+    getCookie = (cName) => {
+        const name = cName + "=";
+        const cDecoded = decodeURIComponent(document.cookie);
+        const cArr = cDecoded.split("; ");
+        let value;
+        cArr.forEach(val => {
+            if (val.indexOf(name) === 0) value = val.substring(name.length);
+        })
+
+        return value;
     }
 
     document.querySelector("#c-button-a").addEventListener("click", () => {
@@ -36,20 +37,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("load", cookieMessage);
 
-    function checkCookie() {
-        let user = getCookie("username");
-        if (user != "") {
-            alert("Welcome again " + user);
-        } else {
-            user = prompt("Please enter your name:", "");
-            if (user != "" && user != null) {
-                setCookie("username", user, 365);
-            }
-        }
-    }
+    //if(localStorage.getItem(cookie) == true) {
+
+    //} else if {
+
+    //} else {
+    //}
+
 
     console.log(localStorage.getItem(cookie));
-
 })
 
 // Add A localStorage Method That Checks If The User Has Allowed Cookies Before, IF NOT COOKIES THEN 'COOKIES?', ELSE COOKIES NO 'COOKIES?'
