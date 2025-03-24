cfb006cf4202174286fdc19b2aeefcc5b32a6dd5
CJ <cjthejuggler@gmail.com>
2025-03-24 19:34:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("JS Loaded")
-    setCookie = (cName, cValue, expDays) => {
-        let date = new Date();
-        date.setTime(date.getTime() + (expDays*24*60*60*1000));
-        const expires = "expires=" + date.toUTCString();
-        document.cookie = cName + "=" + cValue + "; " + expires + "path=/";
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
-    
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    document.querySelector("#c-button-a").addEventListener("click", () => {
+        document.querySelector("#cookies").style.display = "none";
+        document.querySelector(".blur-box").style.display = "none";
+        setCookie("cookie", true, 90);
+        console.log("Cookies Logged");
+    })
+
     cookieMessage = () => {
         if (!getCookie("cookie"))
             document.querySelector("#cookie").style.display = "block";
     }
-    
+
     window.addEventListener("load", cookieMessage);
 
-    if(localStorage.getItem(setCookie) === null) {
-
-        document.querySelector("#c-button-a").addEventListener("click", () => {
-            document.querySelector("#cookies").style.display = "none";
-            document.querySelector(".blur-box").style.display = "none";
-            setCookie("cookie", true, 90);
-            console.log("Cookies Logged");
-        })
-
-        
-
-    }else {
-        getCookie = (cName) => {
-            const name = cName + "=";
-            const cDecoded = decodeURIComponent(document.cookie);
-            const cArr = cDecoded.split("; ");
-            let value;
-            cArr.forEach(val => {
-                if(val.indexOf(name) === 0) value = val.substring(name.length);
-            })
-        
-            return value;
+    function checkCookie() {
+        let user = getCookie("username");
+        if (user != "") {
+            alert("Welcome again " + user);
+        } else {
+            user = prompt("Please enter your name:", "");
+            if (user != "" && user != null) {
+                setCookie("username", user, 365);
+            }
         }
     }
+
 })
 
 // Add A localStorage Method That Checks If The User Has Allowed Cookies Before, IF NOT COOKIES THEN 'COOKIES?', ELSE COOKIES NO 'COOKIES?'
 
 // Run Github As A Test
+
